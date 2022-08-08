3a4ad05e6cd24471d686d83253c3bad6ef315399
HarryHandoko <harrydoko30@gmail.com>
2022-08-08 14:10:12 +0700

    New

--- lib/View/TabDashboard/edit_akun.dart
+++ lib/View/TabDashboard/edit_akun.dart
@@ -1,147 +1,210 @@
 import 'dart:convert';
 import 'package:customer/Service/API/api.dart';
+import 'package:customer/View/Components/appProperties.dart';
+import 'package:customer/View/Pages/home.dart';
+import 'package:customer/View/Router/dashboard.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:http/http.dart' as http;
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class EditAkun extends StatefulWidget {
   @override
+  EditAkun({Key? key}) : super(key: key);
   State<EditAkun> createState() => _EditAkunState();
 }
 
 class _EditAkunState extends State<EditAkun> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        title: Row(
-          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            GestureDetector(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              // ignore: avoid_unnecessary_containers
-              child: Container(
-                height: MediaQuery.of(context).size.height / 25,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(3),
-                  color: Colors.blue[50],
-                ),
-                child: Center(
-                  child: Icon(
-                    Icons.arrow_back_ios_outlined,
-                    color: Colors.black,
-                    size: 20,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 100,
-            ),
-            GestureDetector(
-              onTap: () {
-                // dataku();
-                getDataCustomer();
-              },
-              child: Text(
-                'Edit Akun',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontFamily: 'comfortaa',
-                ),
-              ),
-            ),
-            //
-            //
-            // Column(
-            //   // ignore: prefer_const_literals_to_create_immutables
-            //   children: [
-            //     Icon(
-            //       Icons.add_alert_rounded,
-            //       color: Colors.white,
-            //     ),
-            //   ],
-            // )
-          ],
-        ),
-        automaticallyImplyLeading: false,
-        // backgroundColor: Colors.transparent,
-        // shape:
-        //     RoundedRectangleBorder(borderRadius: BorderRadius.circular(35)),
-      ),
-      body: Container(
-        // width: MediaQuery.of(context).size.width,
-        // height: MediaQuery.of(context).size.height / 10,
-        decoration: BoxDecoration(
-            // color: Colors.blue[50],
-            borderRadius: BorderRadius.circular(25)),
-        child: Padding(
-          padding: const EdgeInsets.only(left: 10.0, right: 10, top: 15),
-          child: TextField(
-            // controller: email,
-            // textAlign: TextAlign.left,
-            // ignore: unnecessary_new
-            decoration: new InputDecoration(
-              fillColor: Colors.blue[50],
-              filled: true,
-              contentPadding: EdgeInsets.only(left: 10, right: 0, top: 20),
-              hintText: 'Tulis review disini',
-              //        prefixIcon:  Padding(
-              //   padding: const EdgeInsets.all(20.0),
-              //   child: Image.asset(
-              //     'gambar/email.png',
-              //     width: 25,
-              //     height: 25,
-              //     fit: BoxFit.fill,
-              //   ),
-              // ),
-              hintStyle: TextStyle(color: Colors.grey, fontSize: 12),
-              border: OutlineInputBorder(
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(10.0),
-                  ),
-                  borderSide: BorderSide.none),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  //
+  List? data;
+  bool loading = false;
   late String customer;
-
-  late String nama;
-  late List profile;
-  getDataCustomer() async {
+  Future getCustomer() async {
     final prefs1 = await SharedPreferences.getInstance();
     customer = prefs1.getString('customer')!;
     var response = await http.get(
-        Uri.parse(Uri.encodeFull(
-            'https://olla.ws/api/customer/v1/customer-profile/')),
+        Uri.parse(
+            Uri.encodeFull('https://olla.ws/api/customer/v1/customer-profile')),
         headers: {
           "Accept": "application/json",
           "x-token-olla": KEY.APIKEY,
           "Authorization": 'Bearer $customer',
         });
-    //
-    setState(() {
-      var converDataToJson = json.decode(response.body);
-      print(converDataToJson);
-      String namaa = converDataToJson['data'];
-      // print(profile);
-      print(namaa.toString());
-      // data1 = converDataToJson['profile'][0]['name'];
-      // // ignore: avoid_print
-      // print('$data1');
-      // print(nama);
-    });
+
+    if (response.statusCode == 200) {
+      setState(() {
+        var converDataToJson = json.decode(response.body);
+        data = converDataToJson['data']['profile'];
+      });
+    }
     return "Success";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getCustomer().whenComplete(() => setState(() {
+          loading = true;
+        }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!loading) {
+      return Scaffold(
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          // color: Colors.red,
+          child: const Center(
+            child: SpinKitFadingCircle(
+              color: Colors.blue,
+              size: 60.0,
+            ),
+          ),
+        ),
+      );
+    } else {
+      final nameController = TextEditingController(text: data![0]['name']);
+      final phoneController =
+          TextEditingController(text: data![0]['mobile_phone']);
+      final emailController = TextEditingController(text: data![0]['email']);
+      return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          title: Row(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Container(
+                  height: MediaQuery.of(context).size.height / 25,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(3),
+                    color: Colors.blue[50],
+                  ),
+                  child: Center(
+                    child: Icon(
+                      Icons.arrow_back_ios_outlined,
+                      color: Colors.black,
+                      size: 20,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 100,
+              ),
+              GestureDetector(
+                onTap: () {
+                  // dataku();
+                  getCustomer();
+                },
+                child: Text(
+                  'Edit Akun',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontFamily: 'comfortaa',
+                  ),
+                ),
+              ),
+            ],
+          ),
+          automaticallyImplyLeading: false,
+        ),
+        body: Container(
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(25)),
+          child: Padding(
+              padding: const EdgeInsets.all(30),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextFormField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        hintText: "Nama Lengkap Anda",
+                        border: InputBorder.none,
+                        prefixIcon: Padding(
+                          padding:
+                              EdgeInsets.only(), // add padding to adjust icon
+                          child: Icon(
+                            FeatherIcons.user,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    TextFormField(
+                      controller: phoneController,
+                      decoration: InputDecoration(
+                        hintText: "Nomor Handphone",
+                        border: InputBorder.none,
+                        prefixIcon: Padding(
+                          padding:
+                              EdgeInsets.only(), // add padding to adjust icon
+                          child: Icon(
+                            FeatherIcons.phone,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    TextFormField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        hintText: "Email",
+                        border: InputBorder.none,
+                        prefixIcon: Padding(
+                          padding:
+                              EdgeInsets.only(), // add padding to adjust icon
+                          child: Icon(
+                            FeatherIcons.mail,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height * 0.08,
+                        decoration: BoxDecoration(
+                            color: primary,
+                            borderRadius: BorderRadius.circular(25)),
+                        child: Center(
+                            child: Text('Simpan',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500))),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Dashboard()),
+                        );
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: Container(
+                          alignment: Alignment.centerRight,
+                          child: Text('Ganti Password'),
+                        ),
+                      ),
+                    )
+                  ])),
+        ),
+      );
+    }
   }
 }
